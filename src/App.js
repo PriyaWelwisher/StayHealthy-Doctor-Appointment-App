0774e1202d3f9d52170c0f455085d4f53692ae90
PriyaWelwisher <senthilkumarpriyadharshini94@gmail.com>
2024-11-08 15:12:33 +0530

    Completed setup is done based on Public and Protected Route

--- src/App.js
+++ src/App.js
@@ -5,21 +5,29 @@ import Register from "./pages/Register";
 import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
 import { useSelector } from "react-redux";
+import ProtectedRoute from "./components/ProtectedRoute";
+import PubliceRoute from "./components/PublicRoute";
 function App() {
-  const {loading} = useSelector((state) => state.alerts);
+  const { loading } = useSelector((state) => state.alerts);
   return (
     <BrowserRouter>
       {loading && (
-      <div className="spinner-parent">
-      <div class="spinner-border" role="status">
-  
-</div>
-      </div> )}
+        <div className="spinner-parent">
+          <div class="spinner-border" role="status"></div>
+        </div>
+      )}
       <Toaster position="top-center" reverseOrder={false} />
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<publicRoute><Login /></publicRoute>} />
+        <Route path="/register" element={<publicRoute><Register /></publicRoute>} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
